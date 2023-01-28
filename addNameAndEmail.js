2de25e12f7ae1ef2fdc3590a0fb38e2c4df70cd1
Jason Alvarez <73498750+toooooby64@users.noreply.github.com>
2023-01-27 23:21:00 -0800

    Update addNameAndEmail.js

--- addNameAndEmail.js
+++ addNameAndEmail.js
@@ -3,7 +3,7 @@ const email = sessionStorage.getItem('email');
 
 console.log(fname)
 console.log(email)
-
+console.log("pls change")
 
 document.getElementById('insertFirstName').innerHTML = fname;
 document.getElementById('insertEmail').innerHTML = email;
